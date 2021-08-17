698f8b616ea7e44df98184ad05338b4eac39f2fe
Lachie Ayres <lachieayres11@gmail.com>
2021-08-17 11:33:56 +0000

    Adding Weeks 4 and 5

--- week 4/guessingGame.sh
+++ week 4/guessingGame.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 Green='\033[0;32m'
-Red='\033[0;31m'
-NC='\033[0m'
 
 echo "Let's play a number guessing game!"
-getNumber() 
-    while (( $1 -eq 42 )); do
-        echo -e " ${Green} Correct!"
-     
+echo "Guess a number between 1 and 100"
+
+read REPLY
+
+if [[ $REPLY -eq 42 ]]; then
+      echo "Correct!"
+      fi
+
+    while (( $REPLY != 42 )); do
+
+
+       if [[ $REPLY -lt 1 || $REPLY -gt 100 ]]; then
+
+        echo "Input must be between 1 and 100"
+      fi
+
+       if [[ $REPLY -lt 42 && $REPLY -gt 0 ]]; then
+
+        echo "Too Low!"
+
+       elif [[ $REPLY -gt 42 && $REPLY -lt 100 ]]; then
+
+        echo "Too High!"
+
+          
+      fi
+      
+        read REPLY
+
+        if [[ $REPLY -eq 42 ]]; then
+      echo "Correct!"
+      fi
+
+
     done
-
-if [ $1 -gt 42 ]
-    then
-    printError "Too high"   
-
-elif [ $1 -lt 42 ]
-    printError "Too low"   
-
-fi
-
-getNumber "Guess a number between 1 and 100" 1 100
---
+++ week 5/regexes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+grep -r "sed"
+grep -r -i "^m"
+grep -r "\d\d\d "
+grep -r -w 'echo '
